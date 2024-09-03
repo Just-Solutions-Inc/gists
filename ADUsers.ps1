648f2cd4b9cf345d51cccc0a848b3db774494775
josiahjsi <139889399+josiahjsi@users.noreply.github.com>
2024-09-03 13:10:42 -0400

    Create ADUsers.ps1
    initial commit for ADUsers.ps1

---
+++ ADUsers.ps1
@@ -0,0 +1,24 @@
+$dateFormat = 'yyyy-MM-dd_HH:mm'
+$users = get-aduser -filter {enabled -eq $true} -Properties lastLogonDate,passwordLastSet,whenCreated,whenChanged,lastBadPasswordAttempt,lockedOut,memberOf |
+    Sort-Object lastLogonDate
+
+function formatDate($date) {
+    if ($date) {
+        return $date.ToString($dateFormat)
+    }
+}
+
+$table = $users | ForEach-Object {
+    [pscustomobject]@{
+        Name            = $_.Name
+        Username        = $_.sAMAccountname
+        LastLogin       = formatDate($_.lastLogonDate)
+        PasswordLastSet = formatDate($_.passwordLastSet)
+        AccountCreated  = $_.whenCreated.ToString($dateFormat)
+        LastModified    = formatDate($_.whenChanged)
+        LastBadPW       = formatDate($_.lastBadPasswordAttempt)
+        AccountLocked   = $_.lockedout
+        Groups          = ($_.memberof | Sort-Object) -replace 'cn=|,(ou|cn)=.+|\\' -join ', '
+    }
+} |
+    export-csv "C:\ADUsers_$(Get-Date -format "yyyy-MM-dd").csv" -NoTypeInformation
